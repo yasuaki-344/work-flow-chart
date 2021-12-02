40fdb55b2157d52be4d9cdcae8340f4e88afd73a
yasuaki-344 <ikausay.443@gmail.com>
2021-12-02 23:06:44 +0900

    Add react flow

--- work-flow-chart-client/src/App.tsx
+++ work-flow-chart-client/src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactFlow from "react-flow-renderer";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const elements = [
+    {
+      id: "1",
+      type: "input",
+      data: { label: "Node 1" },
+      position: { x: 250, y: 5 },
+    },
+    // you can also pass a React Node as a label
+    {
+      id: "2",
+      data: { label: <div>Node 2</div> },
+      position: { x: 100, y: 100 },
+    },
+    { id: "e1-2", source: "1", target: "2", animated: true },
+  ];
+
+  console.log(elements);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactFlow elements={elements} style={{ width: "100%", height: "500px" }} />
   );
-}
+};
 
 export default App;
