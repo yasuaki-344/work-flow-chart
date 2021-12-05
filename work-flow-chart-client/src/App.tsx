d43d0d017a4b4daa3c452ab279865411851b802c
yasuaki-344 <ikausay.443@gmail.com>
2021-12-05 21:18:37 +0900

    Extract DragPanel from App.tsx

--- work-flow-chart-client/src/App.tsx
+++ work-flow-chart-client/src/App.tsx
@@ -11,6 +11,7 @@ import ReactFlow, {
   MiniMap,
   ReactFlowProvider,
 } from "react-flow-renderer";
+import { DragPanel } from "./DragPanel";
 import "./App.css";
 
 const initialElements = [
@@ -47,42 +48,6 @@ const initialElements = [
   },
   { id: "e2-3", source: "2", target: "3", type: "smoothstep" },
 ];
-
-const DragPanel = () => {
-  const onDragStart = (event: any, nodeType: any) => {
-    event.dataTransfer.setData("application/reactflow", nodeType);
-    event.dataTransfer.effectAllowed = "move";
-  };
-
-  return (
-    <aside>
-      <div className="description">
-        You can drag these nodes to the pane on the right.
-      </div>
-      <div
-        className="dndnode input"
-        onDragStart={(event) => onDragStart(event, "input")}
-        draggable
-      >
-        Input Node
-      </div>
-      <div
-        className="dndnode"
-        onDragStart={(event) => onDragStart(event, "default")}
-        draggable
-      >
-        Default Node
-      </div>
-      <div
-        className="dndnode output"
-        onDragStart={(event) => onDragStart(event, "output")}
-        draggable
-      >
-        Output Node
-      </div>
-    </aside>
-  );
-};
 
 let id = 0;
 const getId = () => `dndnode_${id++}`;
